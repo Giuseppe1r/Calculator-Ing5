63579335cff86da7a0a0531fdbec18cb37e90a28
Giuseppe1r <giuseppeportocarrero975@gmail.com>
2026-09-01 22:37:51 -0500

    prueba2

--- simulacion-despliegue-devops-main/frontend/ing5-t1/src/App.jsx
+++ simulacion-despliegue-devops-main/frontend/ing5-t1/src/App.jsx
@@ -57,7 +57,7 @@ function App() {
 
   return (
     <main className="app">
-      <h1> prueba</h1>
+      <h1> por que te mientes?</h1>
       <p className="subtitulo">Simulacion de despliegue DevOps &mdash; React + Spring Boot + MySQL</p>
 
       <section className="tarjeta">
